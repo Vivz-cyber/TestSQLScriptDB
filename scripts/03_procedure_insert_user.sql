2d528d5661f346c4fab9af0bacd04e48c80d5715
Vivek Vatsa <72134769+Vivz-cyber@users.noreply.github.com>
2025-05-08 07:11:01 +0530

    Create 03_procedure_insert_user.sql

---
+++ scripts/03_procedure_insert_user.sql
@@ -0,0 +1,15 @@
+DELIMITER $$
+
+CREATE PROCEDURE sp_insert_user (
+    IN in_firstname VARCHAR(50),
+    IN in_lastname VARCHAR(50),
+    IN in_username VARCHAR(50),
+    IN in_phone VARCHAR(15),
+    IN in_email VARCHAR(100)
+)
+BEGIN
+    INSERT INTO user_table (firstname, lastname, username, phone, email)
+    VALUES (in_firstname, in_lastname, in_username, in_phone, in_email);
+END$$
+
+DELIMITER ;
