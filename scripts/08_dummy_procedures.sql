26222a30480cb6aa597a477bb7eff3dbeb3f0360
Vivek Vatsa <72134769+Vivz-cyber@users.noreply.github.com>
2025-05-08 07:13:40 +0530

    Create 08_dummy_procedures.sql

---
+++ scripts/08_dummy_procedures.sql
@@ -0,0 +1,17 @@
+DELIMITER $$
+
+CREATE PROCEDURE sp_get_all_products()
+BEGIN
+    SELECT * FROM product;
+END$$
+
+CREATE PROCEDURE sp_place_order (
+    IN in_product_id INT,
+    IN in_quantity INT
+)
+BEGIN
+    INSERT INTO orders (product_id, quantity, order_date)
+    VALUES (in_product_id, in_quantity, CURDATE());
+END$$
+
+DELIMITER ;
