31c836fb72ed6d7153862a817214d7eb14edcfbf
Vivek Vatsa <72134769+Vivz-cyber@users.noreply.github.com>
2025-05-08 07:12:49 +0530

    Create 06_procedure_save_repo_metadata.sql

---
+++ scripts/06_procedure_save_repo_metadata.sql
@@ -0,0 +1,19 @@
+DELIMITER $$
+
+CREATE PROCEDURE sp_save_repo_metadata (
+    IN in_username VARCHAR(50),
+    IN in_email VARCHAR(100),
+    IN in_repo_name VARCHAR(255),
+    IN in_local_path TEXT,
+    IN in_github_url TEXT,
+    IN in_build_type VARCHAR(50)
+)
+BEGIN
+    INSERT INTO repo_metadata (
+        username, email, repo_name, local_path, github_url, build_type
+    ) VALUES (
+        in_username, in_email, in_repo_name, in_local_path, in_github_url, in_build_type
+    );
+END$$
+
+DELIMITER ;
